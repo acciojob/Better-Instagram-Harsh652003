e4f0de777cbcd40e0199f9f27c6e9b8f631d821b
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-11 01:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,27 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     image.addEventListener('drop', drop);
   });
 
-function dragStart(e) {
-  console.log("dragStart is triggered");
-  e.dataTransfer.setData('text/plain', e.currentTarget.id);
-}
+  function dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+  }
+
   function dragOver(e) {
     e.preventDefault(); // Prevent default to allow drop
   }
-function drop(e) {
-  e.preventDefault();
-  const id = e.dataTransfer.getData('text');
-  const draggableElement = document.getElementById(id);
-  const dropzone = e.target;
 
-  if (dropzone.classList.contains('image')) {
-    // Extract the URLs from the backgroundImage property
-    let urlRegex = /url\("([^"]*)"\)/;
-    let draggableUrl = draggableElement.style.backgroundImage.match(urlRegex)[1];
-    let dropzoneUrl = dropzone.style.backgroundImage.match(urlRegex)[1];
-    // Swap the background images
-    draggableElement.style.backgroundImage = `url("${dropzoneUrl}")`;
-    dropzone.style.backgroundImage = `url("${draggableUrl}")`;
+  function drop(e) {
+    e.preventDefault();
+    const id = e.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = e.target;
+
+    if (dropzone.classList.contains('image')) {
+      const parent = dropzone.parentNode;
+      parent.insertBefore(draggableElement, dropzone.nextSibling);
+    }
   }
-}
 });
