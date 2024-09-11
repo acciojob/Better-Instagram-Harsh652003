96315a9176c84d8a1274aabd744b5b82d516cc0f
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-11 22:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,24 +6,28 @@ document.addEventListener('DOMContentLoaded', () => {
     image.addEventListener('dragover', dragOver);
     image.addEventListener('drop', drop);
   });
+let draggedImage;
 
-  function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
+function dragStart(e) {
+  draggedImage = e.target.style.backgroundImage;
+  e.dataTransfer.setData('text/plain', e.target.id);
+}
+
+	function dragOver(e) {
+    e.preventDefault(); // This is necessary to allow a drop
+}
+	
+function drop(e) {
+  e.preventDefault();
+  const id = e.dataTransfer.getData('text');
+  const draggableElement = document.getElementById(id);
+  const dropzone = e.target;
+
+  if (dropzone.classList.contains('image')) {
+    // Swap the backgroundImage of the draggedElement and the dropzone
+    const tempImage = dropzone.style.backgroundImage;
+    dropzone.style.backgroundImage = draggedImage;
+    draggableElement.style.backgroundImage = tempImage;
   }
-
-  function dragOver(e) {
-    e.preventDefault(); // Prevent default to allow drop
-  }
-
-  function drop(e) {
-    e.preventDefault();
-    const id = e.dataTransfer.getData('text');
-    const draggableElement = document.getElementById(id);
-    const dropzone = e.target;
-
-    if (dropzone.classList.contains('image')) {
-      const parent = dropzone.parentNode;
-      parent.insertBefore(draggableElement, dropzone.nextSibling);
-    }
-  }
+}
 });
